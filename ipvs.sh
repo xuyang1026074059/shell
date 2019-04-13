a0b01d6d27847bcfcceb5969035ef796d012f099
root <1026074059@qq.com>
2019-04-13 11:17:32 +0800

    k8s > 1.11 start to use ipvs

---
+++ ipvs.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+set -x
+set -e
+
+module=(
+   ip_vs
+   ip_vs_lc
+   ip_vs_wlc
+   ip_vs_rr
+   ip_vs_wrr
+   ip_vs_lblc
+   ip_vs_lblcr
+   ip_vs_dh
+   ip_vs_sh
+   ip_vs_fo
+   ip_vs_nq
+   ip_vs_sed
+   ip_vs_ftp
+)
+
+for kernel_module in ${module[@]};
+do
+    /sbin/modinfo -F filename $kernel_module |  grep -v ERROR && echo $kernel_module  >> /etc/modules-load.d/ipvs.conf
+done
